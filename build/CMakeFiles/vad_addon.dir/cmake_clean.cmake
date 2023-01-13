ae8577a57ad2a0520d155332ddda122be24a7e18
chenqianhe <1278095698@qq.com>
2023-01-13 14:05:26 +0800

    feat: add lib

---
+++ build/CMakeFiles/vad_addon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/Users/chenqianhe/CLionProjects/vad-addon/bin/Darwin/arm64/vad_addon.node"
+  "/Users/chenqianhe/CLionProjects/vad-addon/bin/Darwin/arm64/vad_addon.pdb"
+  "CMakeFiles/vad_addon.dir/src/main.cc.o"
+  "CMakeFiles/vad_addon.dir/src/main.cc.o.d"
+  "CMakeFiles/vad_addon.dir/src/vad.cc.o"
+  "CMakeFiles/vad_addon.dir/src/vad.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vad_addon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
