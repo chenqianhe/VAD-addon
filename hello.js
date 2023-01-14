518a1e802e53f5b32abc3d57ab41a054984f6274
chenqianhe <1278095698@qq.com>
2023-01-14 16:29:58 +0800

    feat: finish win wrapper

--- hello.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-const { vad } = require(path.join(__dirname, 'bin/Darwin/arm64/vad_addon'));
-
-console.log(vad(path.join(__dirname, 'silero_vad.onnx'), path.join(__dirname, 'samples.wav')));
